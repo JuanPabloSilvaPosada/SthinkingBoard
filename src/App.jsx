1fe1ce78e1df01875f68ae35c5dd79c3bd8b15de
Juan <juanpablosilvaposada@gmail.com>
2024-09-25 09:14:14 -0500

    Add new deploy msg

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Ahora se modifico el deploy</p>
+      <p>Ahora se modifico el deploy otra vez</p>
     </>
   )
 }
