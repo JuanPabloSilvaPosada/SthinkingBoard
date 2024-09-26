fc38a55e56140bdca90eab34b49ea3bd9f285c3d
Santiago Restrepo <santiago.restrepoa0512@gmail.com>
2024-09-26 11:37:20 -0500

    test jemr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = ()=> {
     return <>
-        <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
+        <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!!!</h1>
     </>
 }
 
