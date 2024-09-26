9fd48f8c6a3265e9081b6ff111868cc4eff15a1a
Esteban Martinez <mrskesteban@gmail.com>
2024-09-25 22:53:36 -0500

    Test NODIV

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,7 @@ function App() {
   return (
     <>
       <Header/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
