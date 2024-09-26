6453f90a266fe2824c83daf727822596a1c981db
Esteban Martinez <mrskesteban@gmail.com>
2024-09-25 21:57:44 -0500

    Test Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 const Header = ()=> {
-    return <h1 className="text-3xl font-bold underline">Hola!!!</h1>
+    return <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
 }
 
 export default Header
