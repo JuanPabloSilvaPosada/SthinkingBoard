6092c262dbfd609fd52db1167ec3729610585ab6
Juan <juanpablosilvaposada@gmail.com>
2024-09-25 22:13:03 -0500

    test pull

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 const Header = ()=> {
-    return <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
+    return <>
+        <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
+        <h2 className="text-2xl font-bold">Donde solo Pablo coge la pala</h2>
+    </>
 }
 
 export default Header
