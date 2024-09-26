a1bb8294eeb16d3c267e6c64adfbfe18e557fb53
Esteban Martinez <mrskesteban@gmail.com>
2024-09-25 22:15:50 -0500

    Test Santinea

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 const Header = ()=> {
     return <>
         <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
-        <h2 className="text-2xl font-bold">Donde solo Pablo coge la pala</h2>
+        <h2 className="text-2xl font-bold">Donde solo Jhon coge la pala</h2>
+        <h2 className="text-2xl font-bold">Santinea no coge la pala</h2>
     </>
 }
 
