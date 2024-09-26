c3621f29c270a2da06639e7621bab7da23cf6216
Juan <juanpablosilvaposada@gmail.com>
2024-09-25 22:40:06 -0500

    Remove useless h2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 const Header = ()=> {
     return <>
         <h1 className="text-3xl font-bold underline">Bienvenidos a SthinkingBoard!</h1>
-        <h2 className="text-2xl font-bold">Donde solo Jhon coge la pala</h2>
-        <h2 className="text-2xl font-bold">Santinea no coge la pala</h2>
     </>
 }
 
