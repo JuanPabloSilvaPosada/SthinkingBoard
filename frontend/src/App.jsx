38e800f0ffc4e30da627dfd7b994aac09f1cdfe6
Juan <juanpablosilvaposada@gmail.com>
2024-09-30 22:45:27 -0500

    check deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,6 @@ function App() {
   return (
     <>
       <Header/>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={async() => {
-          const res = await fetch('http://localhost:3000/users')
-          const data = await res.json()
-          console.log(data)
-        }}>
-          Users
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>Ahora se modifico el deploy otra vez</p>
     </>
   )
 }
