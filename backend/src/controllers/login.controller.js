5cbf2e2150768956bb44ec872ced1468735027b6
Juan <juanpablosilvaposada@gmail.com>
2024-10-03 17:55:53 -0500

    Add comments on the code

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -1,5 +1,5 @@
-import db from '../db.js'; // Importar la conexión a la base de datos
-import bcrypt from 'bcrypt'; // Importar bcrypt
+import db from '../db.js';
+import bcrypt from 'bcrypt';
 
 export const loginUser = async (req, res) => {
   const { identifier, password } = req.body;
@@ -13,20 +13,21 @@ export const loginUser = async (req, res) => {
     if (result.rows.length > 0) {
       const user = result.rows[0];
 
-      // Comparar la contraseña proporcionada con la almacenada
+      // Compare hashed password
       const match = await bcrypt.compare(password, user.password);
       if (match) {
-        // Contraseña correcta
+        // Correct password
         res.status(200).json({ message: 'Inicio de sesión exitoso', userId: user.id });
       } else {
-        // Contraseña incorrecta
+        // Incorrect password
         res.status(401).json({ message: 'Credenciales incorrectas' });
       }
     } else {
-      // Usuario no encontrado
+      // User not found
       res.status(401).json({ message: 'Credenciales incorrectas' });
     }
   } catch (error) {
+    // Servidor down
     res.status(500).json({ message: 'Error en el servidor', error });
   }
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import loginRoutes from './login.routes.js'; // Importar las rutas de login
+import loginRoutes from './login.routes.js';
 
 const router = express.Router();
 
-// Usar las rutas de login
-router.use('/auth', loginRoutes); // Prefix para las rutas de autenticación
+// Login Route with auth
+router.use('/auth', loginRoutes);
 
 export default router;
